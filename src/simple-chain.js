034ce5a453412c930c7545ce159215a39be96484
Svetlana <svezan@mail.ru>
2020-03-22 22:55:30 +0300

    feat: 9/10

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,19 +1,41 @@
 const chainMaker = {
+  list: [],
   getLength() {
-    
+    return this.list.length;
   },
   addLink(value) {
-    console.log('value: ', value);
-
+    if (value === null) {
+    value = 'null';
+  }
+  
+    this.list.push(`( ${value.toString()} )`);
+  
+    return this;
   },
   removeLink(position) {
-    console.log('position: ', position);
+  position = position - 1;
+  
+  if (this.list[position] === undefined) {
+    this.list = [];
+    throw new Error('Illegal position!');
+  }
+  
+  this.list.splice(position, 1);
+  
+    return this;
   },
+
   reverseChain() {
-    
+  this.list = this.list.reverse();
+  
+  return this;
   },
+
   finishChain() {
+    let result = this.list.join('~~');
+    this.list = [];
     
+    return result;
   }
 };
 
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,65 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct) {
+    this.direct = direct === false ? false : true;
+}
+    encrypt(str, key) {
+    const abc_len = 97;
+    const abc_count = 26;
+
+    let res = '';
+    let j = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let letter = str[i].toLowerCase();
+        if (letter.match(/[a-z]/)) {
+            let key_letter = key[j].toLowerCase();
+            let num = letter.charCodeAt() - abc_len + key_letter.charCodeAt() - abc_len;
+            res+= String.fromCharCode((num % abc_count) + abc_len);
+
+                if (j < key.length - 1) {
+                j++;
+                } else {
+                j = 0;
+                }
+            
+        } else {
+        res+= letter;
+        }
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    return this.direct ? res.toUpperCase() : res.toUpperCase().split('').reverse().join('');
+    }
+
+    decrypt(str, key) {
+    const abc_len = 97;
+    const abc_count = 26;
+
+    let res = '';
+    let j = 0;
+
+    for (let i = 0; i < str.length; i++) {
+    let letter = str[i].toLowerCase()
+
+    if (letter.match(/[a-z]/)) {
+        let key_letter = key[j].toLowerCase();
+        let num = letter.charCodeAt() - key_letter.charCodeAt();
+    if (num < 0) {
+        res+= String.fromCharCode(abc_len + abc_count + num);
+    } else {
+    res+= String.fromCharCode(num + abc_len);
+    }
+
+    if (j < key.length - 1) {
+        j++;
+    } else {
+    j = 0;
+    }
+    } else {
+    res+= letter;
+        }
+    }
+
+        return this.direct ? res.toUpperCase() : res.toUpperCase().split('').reverse().join('');
     }
 }
 
